6c9a497419009de92c26c03005cd49eb89475dab
Gammarayzer <raymondjmorgan@gmail.com>
2022-04-19 15:20:39 -0700

    added graphic calculator but still working out mechanics of script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // type out first number 
 
-// then the opeartor
+// then the opertor
 
 // then type out the second number
 
 // then final equals
 
 // keypad selectors
-
+let displayValue = 56565+ '/' + 4444
+document.querySelector('.mainDisplay').innerHTML = displayValue
