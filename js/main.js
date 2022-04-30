2a5ad38ea4fd7c1f557c3f23f20c1d331c5f9eba
Gammaray <raymondjmorgan@gmail.com>
2022-04-29 18:47:38 -0700

    almost done. This is the 3rd attempt and a simplified version of the orignal one I attempted

--- js/main.js
+++ js/main.js
@@ -1,168 +1,187 @@
-// type out first number 
 
-// then the opertor
-
-// then type out the second number
-
-// then final equals
-
-
-// keypad selectors
-// Object Oriented Programming... adjust your code ray.
-
-const calculator = {
- displayValue :'0',
-    equalsTo : 0
-}
-
-
-console.log("message for the user:")
-console.log("the top is your 'display window' and below it is your 'total window'. you can create as long of a string of numbers and operators as you would like, this calculator will do the math for you in a snap! it will follow PEMDAS order of operations but don't use operators back to back... you will only recieved an error (if you are watching the console that is). use the '<x' button to clear your 'main display' and get you back on track! The 'C' button clears everything from memory. with great power comes great responsiblity so use your powers wisely. ")
-console.log("-signed your friendly neighbourhood programmer :)")
-// make they keys work
-document.querySelector(".clear").addEventListener('click', () => {
-    calculator.displayValue = '0'
-    calculator.equalsTo = 0
-    document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-    document.querySelector('.totalDisplay').innerHTML = calculator.equalsTo
+//this is gold and saves the amount of inputs listeners I need :)
+// so this grabs all the buttons at once so there is no need to do for each button. there is a value added to each html tag that it uses but extracting?? deconstructing the object that is created from the listener because guess what it creates an object for the whole action.
+document.querySelector('.calculator-buttons').addEventListener('click', event => {
+    // the event is passed through which is an object the the short code {target} which is a property in side of the newly created event object
+    const {target} = event;
+    //the target is now an object that has a value from html which this extracts turning into a value value. :)
+    const {value} = target;
+    //this cleans up if there is anything else floating around in the html that isn't a button.
+    if (!target.matches('button')) {
+        return;
+    } else {
+        //this pushes that value to the console and then to the object we are working with called the calculator.
+        console.log(value)
+        calculator.keyInput(value)
+       }
 })
 
-document.querySelector(".back").addEventListener('click', () => {
-    (calculator.displayValue.length === 1) ? calculator.displayValue = '0' : calculator.displayValue = calculator.displayValue.slice(0,-1)
-    document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-    console.log("message for user:")
-    console.log("this will backspace delete the value of your 'display window'.")
-    console.log("-signed your friendly neighbourhood programmer :)")
-})
+ 
 
-document.querySelector(".one").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '1': calculator.displayValue += '1'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".two").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '2': calculator.displayValue += '2'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".three").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '3': calculator.displayValue += '3'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".four").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '4': calculator.displayValue += '4'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".five").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '5': calculator.displayValue += '5'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".six").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '6': calculator.displayValue += '6'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-console.log("message for user:")
-console.log(" this is an easter egg for those of you who are paying attention. I love the number six <3")
-console.log("-signed your friendly neighbourhood programmer :)") 
 
-})
-document.querySelector(".seven").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '7': calculator.displayValue += '7'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".eight").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '8': calculator.displayValue += '8'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-document.querySelector(".nine").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '9': calculator.displayValue += '9'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
+ const calculator = {
+    firstInput: "0",
+    secondInput: "0",
+    finalAnswer: "0",
+    operatorUsed: false,
+    whatOperator: "",
 
-document.querySelector(".zero").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '0': calculator.displayValue += '0'
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
+    keyInput(value) {
 
-document.querySelector(".deci").addEventListener('click', () =>{  
-
- repeatedOperator('.')
-  
-    
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-//now the operators 
-
-document.querySelector(".divi").addEventListener('click', () => {    
-    mainDisplayZero('/')
-    repeatedOperator('/')
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-document.querySelector(".multi").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '0': calculator.displayValue += '*'
-    mainDisplayZero('*') 
-    repeatedOperator('*')
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-document.querySelector(".add").addEventListener('click', () => {
-    mainDisplayZero('+')  
-    repeatedOperator('+')
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-document.querySelector(".sub").addEventListener('click', () => {    
-    mainDisplayZero('-')    
-    repeatedOperator('-')
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-document.querySelector(".bracketLeft").addEventListener('click', () => {    (calculator.displayValue ==='0') ?
-calculator.displayValue = '(': calculator.displayValue += '('
-
-    repeatedOperator("(")
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-document.querySelector(".bracketRight").addEventListener('click', () => {    mainDisplayZero(')')
-    repeatedOperator(")")
-document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-})
-
-// now to make the equals do magical things
-
-document.querySelector(".equals").addEventListener('click', () => {   
-    if (calculator.equalsTo == eval(calculator.displayValue))
-        {   calculator.displayValue = calculator.equalsTo
-            document.querySelector('.mainDisplay').innerHTML = calculator.displayValue
-        } else {
-            calculator.equalsTo = eval(calculator.displayValue)
-            document.querySelector('.totalDisplay').innerHTML = calculator.equalsTo
-            console.log("message for user:")
-            console.log(" if you click equal button twice, the total display window will be copied up into the main display window of further calculation fun.")
-            console.log("-signed your friendly neighbourhood programmer :)") 
+        if ((isNaN(value))) {
+            console.log('this is not a number')
+            this.useOperator(value)
+        } else { 
+            console.log('this is a number')
+            this.useNumber(value)
         }
+    },
+
+    useOperator(value ) {
+//still working on this
+        switch (value) {
+            case ".":
+                if ((!this.operatorUsed) && (this.firstInput === "0")) {
+                    
+                    this.firstInput = '0' + value
+                    this.firstInput = this.isANumber(this.firstInput) 
+                    this.displayText(this.firstInput)
+
+                } else if (!this.operatorUsed) {
+        
+                    this.firstInput += value
+                    this.firstInput = this.isANumber(this.firstInput) 
+                    this.displayText(this.firstInput) 
+                    
+        
+                } else if ((this.operatorUsed) && (this.secondInput === "0")) {
+
+                    this.secondInput = '0' + value
+                    this.secondInput = this.isANumber(this.secondInput) 
+                    this.displayText(this.secondInput)
+        
+                } else if (this.operatorUsed) {
+
+                    this.secondInput += value
+                    this.secondInput = this.isANumber(this.secondInput) 
+                    this.displayText(this.secondInput)
+        
+                } else {
+
+                    this.displayText('I do not know what happened')
+
+                };                
+                break;
+            case "+" : 
+                
+                this.operatorUsed = true
+                this.whatOperator = value
+                break;
+            case "-" :
+                this.operatorUsed = true
+                this.whatOperator = value
+                break;
+            case "/" :
+                this.operatorUsed = true
+                this.whatOperator = value
+                break;
+            case "*" :
+                this.operatorUsed = true
+                this.whatOperator = value
+                break;
+            case "=" :
+                this.calulateNumbers()
+                    break;
+            case "AC" :
+                this.firstInput ="0";
+                this.secondInput = "0";
+                this.finalAnswer ="0";
+                this.operatorUsed = false;
+                this.whatOperator = "";
+                this.displayText(this.firstInput);
+
+                    break;
+            case "BK" :
+                if (!this.operatorUsed) {
+                   this.firstInput = this.firstInput.slice(0,-1)
+                   if (this.firstInput === "") {this.firstInput="0"}
+                    this.displayText(this.firstInput)
+                } else {
+                    this.secondInput = this.secondInput.slice(0,-1)
+                    if (this.secondInput === "") {this.secondInput="0"}
+                    this.displayText(this.secondInput)
+                    }
+                    break;              
+            default:
+                break;
+        }
+
+
+    },
+
+    isANumber(value){
+        if (isNaN(value)) {
+            console.log("NOT A NUMBER")
+            let fixed = value.slice(0,-1)
+            return fixed
+        } else {
+            return value
+        }
+
+        
+    },
+
+    useNumber(value) {
+        if ((!this.operatorUsed) && (this.firstInput === "0")) {
+            this.firstInput = value
+            this.displayText(this.firstInput) 
+
+        } else if (!this.operatorUsed) {
+
+        this.firstInput += value 
+        this.displayText(this.firstInput)
+            
+
+        } else if ((this.operatorUsed) && (this.secondInput === "0")) {
+            this.secondInput = value
+            this.displayText(this.secondInput) 
+
+        } else if (this.operatorUsed) {
+        this.secondInput += value
+        this.displayText(this.secondInput)
+
+        } else {
+            this.displayText('I do not know what happened')
+        }
+    },
+
    
-    
 
-})
+    calulateNumbers(value) {
+        this.operatorUsed = false
+        this.finalAnswer = eval(this.firstInput + this.whatOperator + this.secondInput)
+        this.whatOperator = ''
+        this.firstInput = this.finalAnswer
+        this.secondInput = '0'
+        this.displayText(this.finalAnswer)
 
+    },
 
-function mainDisplayZero(str) {
-    (calculator.displayValue ==='0') ?
-    calculator.displayValue = '0': calculator.displayValue += str
+    displayText(text) {
+        document.querySelector('.calculator-screen').value = text
+        console.log(`first ${this.firstInput}`)
+        console.log(`second ${this.secondInput}`)
+        console.log(`final ${this.finalAnswer}`)
+        console.log(`operator ${this.operatorUsed}  ${this.whatOperator}`)
     }
+
+
+
+
+
+
+
    
-    
-
-function repeatedOperator(oper) {
-    let dispEndVal = calculator.displayValue[calculator.displayValue.length -2]
-
-    if (dispEndVal === oper)
-     { return calculator.displayValue = calculator.displayValue.slice(0,-1)
-    } else { return 
-    }
-
-    } 
-
-    // there is an issue of repeat decimals and repeat operators. I will like to solve this problem but might just shift to a simpler system that doesnt have these string issues.
-    
+}
+       
+  
